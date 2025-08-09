52a3b4b468b65f26f61a7472406e79dfb943d879
Nicolas Machuca <tm.nicolasmachuca@gmail.com>
2025-08-09 18:47:50 -0400

    GUARDADO 24 : NEXIO

--- src/app/soporte/page.jsx
+++ src/app/soporte/page.jsx
@@ -19,36 +19,37 @@ export default function () {
       >
         <div className="col-span-2 p-12">
           <div>
-            <h3 className="text-2xl md:text-4xl">
+            <h3 className="text-sm md:text-4xl">
                <strong className="text-purple-500 text-4xl md:text-6xl">Desarrollo y Soporte</strong><br /><br />Aquí encontrarás asistencia
               rápida y personalizada para resolver cualquier duda o problema.
-            </h3><br /><br />
+            </h3><br />
           </div>
 
           <div>
-            <h4 className=" md:text-2xl font-bold">WhatsApp : +569 32239282 </h4>
+            <h4 className=" textoNormalResposivo font-bold">WhatsApp : <strong className="text-cyan-300">+569 32239282 </strong></h4>
           </div>
             <div>
-            <h4 className="md:text-2xl font-bold" >Correo electrónico:  desarrollo@nexiodevelopers.com</h4>
+            <h4 className="textoNormalResposivo font-bold" >Correo electrónico:  <strong className="text-cyan-300">desarrollo@nexiodevelopers.com</strong></h4>
           </div>
           <div>
-            <br /><br /><br />
-            <p className="md:text-2xl font-bold" >Puedes dejarnos un correo contandonos un problema y te contactaremos en breve</p>
-            <br /><br />
+            <br />
+            <p className="textoNormalResposivo" >Puedes dejarnos un correo contandonos un problema y te contactaremos en breve</p>
+            <br />
 
-            <h4 className=" md:text-2xl font-bold" >¿Cual es tu Nombre?</h4>
-            <br /><input className="bg-black border-2 border-purple-500 rounded-2xl w-120 h-12 text-2xl" type="text" name="nombre" id="nombre" /><br /><br />
-            <h4 className="md:md:text-2xl font-bold" >¿Cual es el nombre de tu empresa?</h4>
-                        <br /><input className="bg-black border-2 border-purple-500 rounded-2xl w-120 h-12 md:text-2xl" type="text" name="nombre" id="nombre" /><br /><br />
+            <h4 className=" textoBoldResponsivo" >¿Cual es tu Nombre?</h4>
+            <input className="inputMorado" type="text" name="nombre" id="nombre" /><br /><br />
 
-            <h4 className="md:md:text-2xl font-bold">¿Cual es tu numero de telefono ?</h4>
-                        <br /><input className="bg-black border-2 border-purple-500 rounded-2xl w-120 h-12 md:text-2xl" type="tel" name="nombre" id="nombre" /><br /><br />
+            <h4 className="textoBoldResponsivo" >¿Cual es el nombre de tu empresa?</h4>
+            <input className="inputMorado" type="text" name="nombre" id="nombre" /><br /><br />
 
-            <h4 className="md:text-2xl font-bold">¿Porfavor indicanos tu correo?</h4>
-                        <br /><input className="bg-black border-2 border-purple-500 rounded-2xl w-120 h-12 md:text-2xl" type="email" name="nombre" id="nombre" /><br /><br />
+            <h4 className="textoBoldResponsivo">¿Cual es tu numero de telefono ?</h4>
+            <input className="inputMorado" type="tel" name="nombre" id="nombre" /><br /><br />
 
-            <h4 className="md:text-2xl font-bold">Cuentanos que problema tienes</h4><br />
-                       <textarea className="bg-black border-2 border-purple-500 rounded-2xl w-120 h-60 md:text-2xl" name="" id=""></textarea><br /><br /><br />
+            <h4 className="textoBoldResponsivo">¿Porfavor indicanos tu correo?</h4>
+            <input className="inputMorado" type="email" name="nombre" id="nombre" /><br /><br />
+
+            <h4 className="textoBoldResponsivo">Cuentanos que problema tienes</h4>
+                       <textarea className="textAreaMorado" name="" id=""></textarea><br /><br /><br />
                        <BotonMui contenido={'Enviar Consulta'} /> 
 
           </div><br />
@@ -62,7 +63,20 @@ export default function () {
           width={'300'}
           className="rounded-4xl"
          ></Image><br /><br /><br />
-        
+             <Image
+          alt="soporte"
+          src={'/soporte2.png'}
+          height={'500'}
+          width={'300'}
+          className="rounded-4xl hidden md:block"
+         ></Image><br /><br /><br />
+                     <Image
+          alt="soporte"
+          src={'/soporte3.png'}
+          height={'500'}
+          width={'300'}
+          className="rounded-4xl hidden md:block"
+         ></Image><br /><br /><br />
         </div>
       </div>
     </div>
